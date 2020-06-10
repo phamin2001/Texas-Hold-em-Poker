856a8b4ae41aed795bf232f0155fa33b6521875a
phamin2001 <amin.pahlavani@gmail.com>
2020-06-10 00:48:25 -0600

    The Card Class II: String Representation and Technical Representation

--- tests/test_card.py
+++ tests/test_card.py
@@ -10,3 +10,11 @@ class CardTest(unittest.TestCase):
     def test_has_suit(self):
         card = Card(rank="2", suit="Clubs")
         self.assertEqual(card.suit, "Clubs")
+
+    def test_has_string_representation_with_rank_suit(self):
+        card = Card("5", "Diamonds")
+        self.assertEqual(str(card), "5 of Diamonds")
+
+    def test_has_technical_representation(self):
+        card = Card("5", "Diamonds")
+        self.assertEqual(repr(card), "Card('5', 'Diamonds')")
