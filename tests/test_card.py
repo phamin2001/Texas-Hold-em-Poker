bc1caf7978f79cdb14005e9517eeab98af8e63a5
phamin2001 <amin.pahlavani@gmail.com>
2020-06-10 00:19:52 -0600

    The Card Class I: Adding Initial Attributes

---
+++ tests/test_card.py
@@ -0,0 +1,12 @@
+import unittest
+from poker.card import Card
+
+
+class CardTest(unittest.TestCase):
+    def test_has_rank(self):
+        card = Card(rank="Queen", suit="Hearts")
+        self.assertEqual(card.rank, "Queen")
+
+    def test_has_suit(self):
+        card = Card(rank="2", suit="Clubs")
+        self.assertEqual(card.suit, "Clubs")
