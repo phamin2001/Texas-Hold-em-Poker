c9d19cc6a685b4a8bdf39730ae4c5d991009f0df
phamin2001 <amin.pahlavani@gmail.com>
2020-06-20 19:42:26 -0600

    Implementing Fake Betting in the Player and GameRound Classes

--- tests/test_player.py
+++ tests/test_player.py
@@ -29,8 +29,15 @@ class PlayerTest(unittest.TestCase):
         mock_hand = MagicMock()
         player = Player(name="Kimberly", hand=mock_hand)
 
-        cards = [Card(rank="Ace", suit="Spades"), Card(rank="Queen", suit="Diamonds")]
+        cards = [
+            Card(rank="Ace", suit="Spades"),
+            Card(rank="Queen", suit="Diamonds")
+        ]
 
         player.add_cards(cards)
 
         mock_hand.add_cards.assert_called_once_with(cards)
+
+    def test_decides_to_continue_or_drop_out_of_game(self):
+        player = Player(name="Sharon", hand=Hand)
+        self.assertEqual(player.wants_to_fold(), False)
