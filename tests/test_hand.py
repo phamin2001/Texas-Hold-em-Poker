fa5e3351434b9874124f5b8e31be0115222194cf
phamin2001 <amin.pahlavani@gmail.com>
2020-07-04 00:15:24 -0600

    The RoyalFlushValidator Class

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -30,19 +30,3 @@ class HandTest(unittest.TestCase):
         hand.add_cards(cards)
 
         self.assertEqual(hand.cards, [six_of_clubs, ace_of_spades])
-
-    def test_figures_out_royal_flush_is_best_rank(self):
-        cards = [
-            Card(rank="10", suit="Clubs"),
-            Card(rank="Jack", suit="Clubs"),
-            Card(rank="Queen", suit="Clubs"),
-            Card(rank="King", suit="Clubs"),
-            Card(rank="Ace", suit="Clubs"),
-        ]
-
-        # TODO: we need to fix age cases with 6 or 7 cards
-
-        hand = Hand()
-        hand.add_cards(cards)
-
-        self.assertEqual(hand.best_rank(), "Royal Flush")
