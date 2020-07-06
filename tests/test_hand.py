7128fee0fb2832570db856c4773be6323717bcbd
phamin2001 <amin.pahlavani@gmail.com>
2020-07-06 12:36:09 -0600

    Refactoring the Hand Class

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -2,6 +2,7 @@ import unittest
 
 from poker.card import Card
 from poker.hand import Hand
+from poker.validators import PairValidator
 
 
 class HandTest(unittest.TestCase):
@@ -30,3 +31,19 @@ class HandTest(unittest.TestCase):
         hand.add_cards(cards)
 
         self.assertEqual(hand.cards, [six_of_clubs, ace_of_spades])
+
+    def test_interacts_with_validator_to_get_winning_hand(self):
+        class HandWithOneValidator(Hand):
+            VALIDATORS = (PairValidator, )
+
+        ace_of_hearts = Card(rank="Ace", suit="Hearts")
+        ace_of_spades = Card(rank="Ace", suit="Spades")
+        cards = [ace_of_hearts, ace_of_spades]
+
+        hand = HandWithOneValidator()
+        hand.add_cards(cards=cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Pair"
+        )
